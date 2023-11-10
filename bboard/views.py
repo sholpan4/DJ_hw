9ec3b37b5114b573c2926b9768ee5a2fcaa399b4
sholpan4 <b.o.sholpan44@gmail.com>
2023-11-10 11:43:10 +0600

    lesson_01 files

--- bboard/views.py
+++ bboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    return HttpResponse("Hello World!")
